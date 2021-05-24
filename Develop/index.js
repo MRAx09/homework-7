aaad7b4e8fe51433a94f9a0edbe50bf0809151f3
MRAx09 <martin.rae95@gmail.com>
2021-05-23 18:43:54 -0700

    code

--- Develop/index.js
+++ Develop/index.js
@@ -1,31 +1,41 @@
 // TODO: Include packages needed for this application
-const questions = require('inquirer');
+const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [
-    {
-        type: 'input',
-        name: 'name',
-        message: 'input your name:'
-    },
-    {
-        type: 'input',
-        name: 'linkedIn',
-        message: 'input your linkedIn URL:'
-    },
-    {
-        type: 'input',
-        name: 'bio',
-        message: 'input your biography:'
-    },
-];
+const questions = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?:'
+        },
+        {
+            type: 'input',
+            name: 'project',
+            message: 'What is the name of your project?:'
+        },
+        {
+            type: 'input',
+            name: 'info',
+            message: 'Tell us about your project?:'
+        },
+    ]);
+}
+    
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data)
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  questions()
+    .then((answers) => writeToFile('README.md', answers))
+    .then(() => console.log('Successfully wrote to index.html'))
+    .catch((err) => console.error(err));
+}
 
 // Function call to initialize app
 init();
