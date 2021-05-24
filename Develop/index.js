999c7deece3560ad79d9f7c21b42b37824358812
MRAx09 <martin.rae95@gmail.com>
2021-05-23 17:57:47 -0700

    Update index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,25 @@
 // TODO: Include packages needed for this application
+const questions = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'name',
+        message: 'input your name:'
+    },
+    {
+        type: 'input',
+        name: 'linkedIn',
+        message: 'input your linkedIn URL:'
+    },
+    {
+        type: 'input',
+        name: 'bio',
+        message: 'input your biography:'
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
